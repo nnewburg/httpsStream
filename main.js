20ad8d42d3e51878daf3d9d25722e72f0249bcd2
Nick <pickme1152019@gmail.com>
2019-03-11 17:58:13 +0000

    intial commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-intial commit
+let https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+
+}
